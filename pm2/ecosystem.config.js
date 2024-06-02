44d7a49d67e2078844c51975a7d3e78a90b1111e
Oleh Voloshyn <bazisc.qa@gmail.com>
2024-06-02 13:26:18 +0300

    update pm2 ecosystem file

--- pm2/ecosystem.config.js
+++ pm2/ecosystem.config.js
@@ -2,55 +2,55 @@ module.exports = {
   apps : [
     {
       name: "api-gateway",
-      script: "cd ../../api-gateway && npm run start:dev",
+      script: "cd ../../api-gateway && npm i && npm run start:dev",
     },
     {
       name: "auth",
-      script: "cd ../../auth && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../auth && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "community",
-      script: "cd ../../community && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../community && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "history",
-      script: "cd ../../history && npm run start:dev",
+      script: "cd ../../history && npm i && npm run start:dev",
     },
     {
       name: "library",
-      script: "cd ../../library && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../library && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "search",
-      script: "cd ../../search && npm run start:dev",
+      script: "cd ../../search && npm i && npm run start:dev",
     },
     {
       name: "storage",
-      script: "cd ../../storage && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../storage && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "subscriptions",
-      script: "cd ../../subscriptions && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../subscriptions && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "users",
-      script: "cd ../../users && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../users && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "video-manager",
-      script: "cd ../../video-manager && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../video-manager && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "video-processor",
-      script: "cd ../../video-processor && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../video-processor && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "video-store",
-      script: "cd ../../video-store && npx prisma migrate deploy && npm run start:dev",
+      script: "cd ../../video-store && npm i && npx prisma generate && npx prisma migrate deploy && npm run start:dev",
     },
     {
       name: "web-status",
-      script: "cd ../../web-status && npm run start:dev",
+      script: "cd ../../web-status && npm i && npm run start:dev",
     }
   ]
 }
